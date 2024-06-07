5b5ae09f1cf01fe81f1049a0fb6311d214f303b1
Juel Justine <itsmejueljy@outlook.com>
2024-06-07 14:01:37 +0800

    Added dashboard!

--- app/src/main/java/com/sendiribuat/purrfectpals/FirebaseDbHelper.java
+++ app/src/main/java/com/sendiribuat/purrfectpals/FirebaseDbHelper.java
@@ -3,6 +3,7 @@ package com.sendiribuat.purrfectpals;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,10 +16,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FirebaseDbHelper {
     private FirebaseDatabase db;
+    private FirebaseAuth mAuth;
     private DatabaseReference ref;
     private Context context;
     public FirebaseDbHelper(Context context) {
         db = FirebaseDatabase.getInstance(Constant.FIREBASE_DB_INSTANCE);
+        mAuth = FirebaseAuth.getInstance();
         this.context = context;
     }
     public boolean insertUser(User user, String userId) {
@@ -59,6 +62,28 @@ public class FirebaseDbHelper {
         });
     }
 
+    public void getUserInformation(String userId, final UserCallback userCallback) {
+        ref = db.getReference("users");
+        ref.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    userCallback.onUserCallback(user);
+                } else {
+                    Toast.makeText(context, "User not found", Toast.LENGTH_SHORT).show();
+                    userCallback.onUserCallback(null);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(context, "Failed to get user information: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                userCallback.onUserCallback(null);
+            }
+        });
+    }
+
     public void insertVaccineSchedule(VaccinationSchedule schedule) {
         ref = db.getReference("vaccine_schedules");
         String key = ref.push().getKey();
@@ -89,5 +114,9 @@ public class FirebaseDbHelper {
         void onPetNamesLoaded(String[] petNames);
 
         void onLoadFailed(String errorMessage);
+    }
+
+    public interface UserCallback {
+        void onUserCallback(User user);
     }
 }
--- app/src/main/java/com/sendiribuat/purrfectpals/MainActivity.java
+++ app/src/main/java/com/sendiribuat/purrfectpals/MainActivity.java
@@ -3,6 +3,7 @@ package com.sendiribuat.purrfectpals;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseDbHelper db;
+    FirebaseAuth mAuth;
+    User loggedIn;
+    TextView welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +29,52 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        welcome = findViewById(R.id.welcomeTxt);
+        db = new FirebaseDbHelper(this);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null) {
+            db.getUserInformation(user.getUid(), new FirebaseDbHelper.UserCallback() {
+                @Override
+                public void onUserCallback(User user) {
+                    loggedIn = user;
+                    welcome.setText("Welcome, " + user.getName());
+                }
+            });
+        } else {
+            goToLogin();
+        }
     }
 
     public void goToLogin() {
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
+    }
+
+    public void toVaccine(View view) {
+        startActivity(new Intent(getApplicationContext(), VaccinationActivity.class));
+    }
+
+    public void toPetProfile(View view) {
+        startActivity(new Intent(getApplicationContext(), PetProfile.class));
+        finish();
+    }
+
+    public void toPetSchedule(View view) {
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    public void toFoodRecipe(View view) {
+        startActivity(new Intent(getApplicationContext(), FoodRecipe.class));
+    }
+
+    public void toFeedingSchedule(View view) {
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    public void logoutUser(View view) {
+        mAuth.signOut();
+        goToLogin();
     }
 }
